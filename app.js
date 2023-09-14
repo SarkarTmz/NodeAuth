cbc837f0a6dcb6c13e5e8768aabcf2430a872116
SarkarTmz <105831207+SarkarTmz@users.noreply.github.com>
2023-09-14 23:11:50 +0545

    first commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const { users } = require("./model/index")
 const app = express()
 const bcrypt = require("bcryptjs")
+const { where } = require("sequelize")
 app.set("view engine","ejs")
 
 // database connection 
@@ -28,14 +29,61 @@ app.post("/register", async (req,res)=>{
         return res.send("Please provide email,username,password")
     }
 
-   await  users.create({
-        email : email,
-        username:username,
-        password:bcrypt.hashSync(password,12)
-    })
-    res.send("User Registered Successfully")
+
+                  // aako ko email ko kohi xa ki nae find garnu paryo 
+            const emailExist =    await users.findAll({
+                where  : {
+                    email : email,
+                }
+            })
+            if(emailExist.length > 0 ){
+                res.send("User with that email already registered")
+            }else{
+                
+                // validation from server sid
+            await  users.create({
+                    email : email,
+                    username:username,
+                    password:bcrypt.hashSync(password,12)
+                })
+                res.redirect("/login")
+            }
+            
 
 })
+
+//login
+app.get("/login",(req,res)=>{
+    res.render("login")
+})
+
+
+app.post("/login", async (req,res)=>{
+    // console.log(req.body)
+    const email = req.body.email
+    const password = req.body.password
+
+    const userExist = await users.findAll({
+        where: {
+            email: email
+        }
+    });
+
+
+    if(userExist.length > 0){
+        // pass check gane 
+        const isMatch = bcrypt.compareSync(password, userExist[0].password)
+        console.log(isMatch)
+        if(isMatch){
+            res.send("Login successfully")
+        }else{
+            res.send("Invalid Password")
+        }
+    }else{
+        res.send("Invalid Email")
+    }
+})
+
 
 app.listen(3000,function(){
     console.log("NodeJs project  started at port 3000")
